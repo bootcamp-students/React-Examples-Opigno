86a3cfd3c9e95e9c6fcaa59f60b382b99faedd95
ian.rios <ian.rios@awesomeinc.org>
2021-09-10 10:34:39 -0400

    React Advanced 2 - useState

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InfoCard from '../components/InfoCard'
 
 export default function Home() {
+
+    // setState -> setWord
+    const [word, setWord] = useState("hello")
+
+
+
+    const [num, setNum] = useState(0)
+    const numChanger = () => {
+        setNum(num + 1)
+    }
+
+
+
+
+    const [array, setArray] = useState([])
+    // 1st method
+    const updateArr1 = () => {
+        let newArr = [...array, "added new item"]
+        setArray(newArr)
+    }
+
+    // second method
+    const updateArr2 = () => {
+        setArray([...array, "added new item"])
+    }
+
+    // using prev state
+    const updateArr3 = () => {
+        const setter = prevArray => {
+            return [...prevArray, "new item"]
+        }
+        setArray(setter)
+    }
+    
+    // using prev state inline
+    const updateArr4 = () => {
+        setArray(prevArray => {
+            return [...prevArray, "new item"]
+        })
+    }
 
     return (
         <main role="main">
@@ -9,21 +49,21 @@ export default function Home() {
                 <div className="container">
                     <h1 className="jumbotron-heading">Album example</h1>
                     <p className="lead text-muted">
-                        Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.
+                        {num} <button className="btn btn-primary" onClick={numChanger}>click me</button>
                     </p>
-                    <p>
+                    {/* <p>
                         <a href="#" className="btn btn-primary my-2">Main call to action</a>
                         <a href="#" className="btn btn-secondary my-2">Secondary action</a>
-                    </p>
+                    </p> */}
                 </div>
             </section>
-            <div className="album py-5 bg-light">
+            {/* <div className="album py-5 bg-light">
                 <div className="container">
                     <div className="row">
                         {[...Array(10).keys()].map((item, index) => <InfoCard key={index} />)}
                     </div>
                 </div>
-            </div>
+            </div> */}
         </main>
     )
 }
